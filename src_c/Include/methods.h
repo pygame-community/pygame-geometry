14a5726c08869ba3255062d23b521dc5c3dbc14b
Emc2356 <63981925+Emc2356@users.noreply.github.com>
2022-08-04 17:37:05 +0300

    changed the Line.raycast

--- src_c/Include/methods.h
+++
@@ -1,8 +0,0 @@
-#include <Python.h>
-
-#ifndef PySequence_FAST_CHECK
-#define PySequence_FAST_CHECK(o) (PyList_Check(o) || PyTuple_Check(o))
-#endif /* ~PySequence_FAST_CHECK */
-
-static PyObject *
-pg_geometry_raycast(PyObject *self, PyObject *const *args, Py_ssize_t nargs);
