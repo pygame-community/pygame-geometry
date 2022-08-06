a2eb38a0879827089f5d2249da1cbe5caee607c9
itzpr3d4t0r <103119829+itzpr3d4t0r@users.noreply.github.com>
2022-08-06 18:14:43 +0200

    Added a message that explains how the test operates. added some more tests.

--- benchmarks/GEOMETRY_line_benchmark.py
+++ benchmarks/GEOMETRY_line_benchmark.py
@@ -13,7 +13,17 @@ def random_line():
     return Line(random_pos(), random_pos())
 
 
-CPT = 1_000_000  # Calls Per Test
+# ====================================================
+# Each test consists of a tuple of: (name, call)
+# The name is a string containing the name of the test
+# The call is a string containing the code to be timed
+# every test is run CPT times and the average time is
+# calculated across REP runs
+# the formula is time = (time per CPT calls repeated REP times) / REP
+# ====================================================
+
+CPT = 1_000_000  # Calls per test
+REP = 5  # Repetitions per test
 
 l1 = Line(0, 0, 10, 10)
 l2 = Line(0, 10, 10, 0)
@@ -51,11 +61,17 @@ GLOB = {
 general_test = [
     ("Instatiation", "Line(0, 0, 5, 5)"),
     ("x1 attrib", "l1.x1"),
+    ("x1 attrib set", "l1.x1 = 1"),
     ("y1 attrib", "l1.y1"),
+    ("y1 attrib set", "l1.y1 = 1"),
     ("x2 attrib", "l1.x2"),
+    ("x2 attrib set", "l1.x2 = 1"),
     ("y2 attrib", "l1.y2"),
+    ("y2 attrib set", "l1.y2 = 1"),
     ("a attrib", "l1.a"),
+    ("a attrib set", "l1.a = (1, 1)"),
     ("b attrib", "l1.b"),
+    ("b attrib set", "l1.b = (1, 1)"),
     ("copy", "l1.copy()"),
     ("update line", "l1.update(l2)"),
     ("update 1 tup", "l1.update((1, 1, 3, 3))"),
@@ -108,4 +124,4 @@ TESTS = [
     ("Raycast", raycast_test),
 ]
 
-TestSuite("Geometry Module - Line", TESTS, GLOB, num=CPT).run_suite()
+TestSuite("Geometry Module - Line", TESTS, GLOB, CPT, REP).run_suite()
