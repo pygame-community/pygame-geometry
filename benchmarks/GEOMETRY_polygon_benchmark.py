6fef045187d69f7ff4564349502ed222f68690d8
itzpr3d4t0r <103119829+itzpr3d4t0r@users.noreply.github.com>
2022-08-26 19:45:08 +0200

    made benchmark more compact

--- benchmarks/GEOMETRY_polygon_benchmark.py
+++ benchmarks/GEOMETRY_polygon_benchmark.py
@@ -2,33 +2,6 @@ from benchmark_utils import TestSuite
 
 from geometry import Polygon
 
-p1_i = (50, 50)
-p2_i = (50, 100)
-p3_i = (70, 55)
-p4_i = (100, 23)
-
-p1_f = (50.0, 50.0)
-p2_f = (50.0, 100.0)
-p3_f = (70.0, 55.0)
-p4_f = (100.0, 23.0)
-
-po3 = Polygon([(50, 50), (50, 100), (70, 55)])
-po4 = Polygon([(50, 50), (50, 100), (70, 55), (100, 23)])
-
-GLOB = {
-    "Polygon": Polygon,
-    "po3": po3,
-    "po4": po4,
-    "p1_i": p1_i,
-    "p2_i": p2_i,
-    "p3_i": p3_i,
-    "p4_i": p4_i,
-    "p1_f": p1_f,
-    "p2_f": p2_f,
-    "p3_f": p3_f,
-    "p4_f": p4_f,
-}
-
 # === Tests ===
 # Each test consists of a tuple of: (name, call)
 # The name is a string containing the name of the test
@@ -37,6 +10,20 @@ GLOB = {
 # calculated across REP runs
 # the formula is time = (time per CPT calls repeated REP times) / REP
 # ====================================================
+
+GLOB = {
+    "Polygon": Polygon,
+    "po3": Polygon([(50, 50), (50, 100), (70, 55)]),
+    "po4": Polygon([(50, 50), (50, 100), (70, 55), (100, 23)]),
+    "p1_i": (50, 50),
+    "p2_i": (50, 100),
+    "p3_i": (70, 55),
+    "p4_i": (100, 23),
+    "p1_f": (50.0, 50.0),
+    "p2_f": (50.0, 100.0),
+    "p3_f": (70.0, 55.0),
+    "p4_f": (100.0, 23.0),
+}
 
 creation_tests = [
     ("(3) poly", "Polygon(po3)"),
