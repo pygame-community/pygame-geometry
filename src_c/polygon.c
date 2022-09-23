28e1096389b1bc30eff5654b6d33a17441d0e20d
Mahabbat <73908452+maqa41@users.noreply.github.com>
2022-09-23 23:03:11 +0400

    Polygon functions (#3)
    * collisions and center for polygons
    
    done:poly-poly collisions, poly-line
    to do: poly-rect, poly-circle, poly-line
    
    * update
    
    some fixes of typos and incomplete functionalities
    
    * a bunch of functions
    
    -Added collision functions.
    -Added move and move_ip functions
    
    * binding collision functions to pyObjects
    
    -Bound the polygon collision functions to pyobjects.
    -Added those functions to c_api at geometry.c file
    
    * Update geometry.h
    
    -Forgot to remove the absolute path containing include line
    
    * bug fixes
    
    some bugs are fixed
    
    * huge update
    
    -Collision functions are working now
    
    * Update polygon.c
    
    * Update geometry.c
    
    * Update collisions.h
    
    * Update geometry.h
    
    * Update collisions.c
    
    * Delete polygon_test.py
    
    * added tests and restructured a lot of stuff
    
    -test are added and all of them works
    -restructured a lot of stuff including all constructor functions and so on
    -eliminated some bugs
    
    * Delete polygon_test.py
    
    conflicts with other branches, it will be saved locally at the moment

--- src_c/polygon.c
+++ src_c/polygon.c
@@ -7,6 +7,40 @@
 #include <stddef.h>
 #include <math.h>
 
+static int
+set_polygon_center_coords(pgPolygonBase *polygon)
+{
+    if (!polygon) {
+        return 0;
+    }
+    double sum_x = 0;
+    double sum_y = 0;
+    Py_ssize_t i2; 
+    for (i2 = 0; i2 < polygon->verts_num * 2; i2 += 2) {
+        sum_x += polygon->vertices[i2];
+        sum_y += polygon->vertices[i2 + 1];
+    }
+    polygon->c_x = sum_x / polygon->verts_num;
+    polygon->c_y = sum_y / polygon->verts_num;
+    return 1;
+}
+
+static int
+move_polygon(pgPolygonBase *polygon, double dx, double dy)
+{
+    if (!polygon) {
+        return 0;
+    }
+    Py_ssize_t i2;
+    for (i2 = 0; i2 < polygon->verts_num * 2; i2 += 2) {
+        polygon->vertices[i2] += dx;
+        polygon->vertices[i2 + 1] += dy;
+    }
+    polygon->c_x += dx;
+    polygon->c_y += dy;
+    return 1;
+}
+
 static PyObject *
 pg_tuple_from_values_double(double val1, double val2)
 {
@@ -40,10 +74,12 @@ _pg_polygon_vertices_aslist(pgPolygonBase *poly)
         return NULL;
     }
     Py_ssize_t i;
+    Py_ssize_t i2;
 
     for (i = 0; i < poly->verts_num; i++) {
-        PyObject *tup = pg_tuple_from_values_double(poly->vertices[i * 2],
-                                                    poly->vertices[i * 2 + 1]);
+        i2 = i * 2;
+        PyObject *tup = pg_tuple_from_values_double(poly->vertices[i2],
+                                                    poly->vertices[i2 + 1]);
         if (!tup) {
             Py_DECREF(list);
             return NULL;
@@ -62,9 +98,12 @@ _pg_polygon_vertices_astuple(pgPolygonBase *poly)
         return NULL;
     }
     Py_ssize_t i;
+    Py_ssize_t i2;
+    
     for (i = 0; i < poly->verts_num; i++) {
-        PyObject *tup = pg_tuple_from_values_double(poly->vertices[i * 2],
-                                                    poly->vertices[i * 2 + 1]);
+        i2 = i * 2;
+        PyObject *tup = pg_tuple_from_values_double(poly->vertices[i2],
+                                                    poly->vertices[i2 + 1]);
         if (!tup) {
             Py_DECREF(vertices);
             return NULL;
@@ -94,7 +133,7 @@ pgPolygon_FromObject(PyObject *obj, pgPolygonBase *out)
 
         memcpy(out->vertices, poly->vertices,
                poly->verts_num * 2 * sizeof(double));
-
+        set_polygon_center_coords(out);
         return 1;
     }
 
@@ -104,6 +143,7 @@ pgPolygon_FromObject(PyObject *obj, pgPolygonBase *out)
 
         if (length >= 3) {
             Py_ssize_t i;
+            Py_ssize_t i2;
             out->verts_num = length;
 
             if (!out->vertices) {
@@ -115,12 +155,19 @@ pgPolygon_FromObject(PyObject *obj, pgPolygonBase *out)
                 }
             }
 
+            double x_coord_sum = 0.0;
+            double y_coord_sum = 0.0;
             for (i = 0; i < out->verts_num; i++) {
-                if (!pg_TwoDoublesFromObj(f_arr[i], &(out->vertices[i * 2]),
-                                          &(out->vertices[i * 2 + 1]))) {
+                i2 = i * 2;
+                if (!pg_TwoDoublesFromObj(f_arr[i], &(out->vertices[i2]),
+                                          &(out->vertices[i2 + 1]))) {
                     return 0;
                 }
+                x_coord_sum += out->vertices[i2];
+                y_coord_sum += out->vertices[i2 + 1];
             }
+            out->c_x = x_coord_sum / out->verts_num;
+            out->c_y = y_coord_sum / out->verts_num;
             return 1;
         }
         else if (length == 1) {
@@ -140,23 +187,31 @@ pgPolygon_FromObject(PyObject *obj, pgPolygonBase *out)
 
         if (length >= 3) {
             Py_ssize_t i;
+            Py_ssize_t i2;
+            
             out->verts_num = length;
             out->vertices = PyMem_New(double, length * 2);
             if (!out->vertices) {
                 return 0;
             }
 
+            double x_coord_sum = 0.0;
+            double y_coord_sum = 0.0;
             for (i = 0; i < length; i++) {
+                i2 = i * 2;
                 tmp = PySequence_ITEM(obj, i);
-                if (!pg_TwoDoublesFromObj(tmp, &(out->vertices[i]),
-                                          &(out->vertices[i + 1]))) {
+                if (!pg_TwoDoublesFromObj(tmp, &(out->vertices[i2]),
+                                          &(out->vertices[i2 + 1]))) {
                     Py_DECREF(tmp);
                     return 0;
                 }
+                x_coord_sum += out->vertices[i2];
+                y_coord_sum += out->vertices[i2 + 1];
                 Py_DECREF(tmp);
                 tmp = NULL;
             }
-
+            out->c_x = x_coord_sum / out->verts_num;
+            out->c_y = y_coord_sum / out->verts_num;
             return 1;
         }
         else if (length == 1) {
@@ -212,6 +267,7 @@ pgPolygon_FromObjectFastcall(PyObject *const *args, Py_ssize_t nargs,
     }
     else if (nargs >= 3) {
         Py_ssize_t i;
+        Py_ssize_t i2;
         out->verts_num = nargs;
 
         if (!out->vertices) {
@@ -222,14 +278,19 @@ pgPolygon_FromObjectFastcall(PyObject *const *args, Py_ssize_t nargs,
                 return 0;
             }
         }
-
+        double x_coord_sum = 0.0;
+        double y_coord_sum = 0.0;   
         for (i = 0; i < nargs; i++) {
-            if (!pg_TwoDoublesFromObj(args[i], &(out->vertices[i * 2]),
-                                      &(out->vertices[i * 2 + 1]))) {
+            i2 = i * 2;
+            if (!pg_TwoDoublesFromObj(args[i], &(out->vertices[i2]),
+                                      &(out->vertices[i2 + 1]))) {
                 return 0;
             }
+            x_coord_sum += out->vertices[i2];
+            y_coord_sum += out->vertices[i2 + 1];         
         }
-
+        out->c_x = x_coord_sum / out->verts_num;
+        out->c_y = y_coord_sum / out->verts_num;   
         return 1;
     }
 
@@ -271,6 +332,7 @@ _pg_polygon_subtype_new2(PyTypeObject *type, double *vertices,
                verts_num * 2 * sizeof(double));
 
         polygon_obj->polygon.verts_num = verts_num;
+        set_polygon_center_coords(&(polygon_obj->polygon));
     }
 
     return (PyObject *)polygon_obj;
@@ -284,6 +346,8 @@ pg_polygon_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
     if (self) {
         self->polygon.vertices = NULL;
         self->polygon.verts_num = 0;
+        self->polygon.c_x = 0;
+        self->polygon.c_y = 0;
         self->weakreflist = NULL;
     }
 
@@ -341,7 +405,64 @@ pg_polygon_copy(pgPolygonObject *self, PyObject *_null)
     return pgPolygon_New(&self->polygon);
 }
 
+static PyObject *
+pg_polygon_move(pgPolygonObject *self, PyObject *const *args, Py_ssize_t nargs)
+{
+    double Dx = 0.0, Dy = 0.0;
+
+    if (nargs == 1) {
+        if (!pg_TwoDoublesFromObj(args[0], &Dx, &Dy)) {
+            goto error;
+        }
+    }
+    else if (nargs == 2) {
+        if (!pg_DoubleFromObj(args[0], &Dx) ||
+            !pg_DoubleFromObj(args[1], &Dy)) {
+            goto error;
+        }
+    }
+    else {
+        goto error;
+    }
+
+    move_polygon(&self->polygon, Dx, Dy);
+    return _pg_polygon_subtype_new2(Py_TYPE(self), self->polygon.vertices,
+                                    self->polygon.verts_num);
+error:
+    return RAISE(PyExc_TypeError, "move requires a pair of numbers");
+}
+
+static PyObject *
+pg_polygon_move_ip(pgPolygonObject *self, PyObject *const *args,
+                  Py_ssize_t nargs)
+{
+    double Dx = 0.0, Dy = 0.0;
+
+    if (nargs == 1) {
+        if (!pg_TwoDoublesFromObj(args[0], &Dx, &Dy)) {
+            goto error;
+        }
+    }
+    else if (nargs == 2) {
+        if (!pg_DoubleFromObj(args[0], &Dx) ||
+            !pg_DoubleFromObj(args[1], &Dy)) {
+            goto error;
+        }
+    }
+    else {
+        goto error;
+    }
+
+    move_polygon(&(self->polygon), Dx, Dy);
+    Py_RETURN_NONE;
+
+error:
+    return RAISE(PyExc_TypeError, "move requires a pair of numbers");
+}
+
 static struct PyMethodDef pg_polygon_methods[] = {
+    {"move", (PyCFunction)pg_polygon_move, METH_FASTCALL, NULL},
+    {"move_ip", (PyCFunction)pg_polygon_move_ip, METH_FASTCALL, NULL},
     {"__copy__", (PyCFunction)pg_polygon_copy, METH_NOARGS, NULL},
     {"copy", (PyCFunction)pg_polygon_copy, METH_NOARGS, NULL},
     {NULL, NULL, 0, NULL}};
@@ -358,7 +479,71 @@ pg_polygon_get_vertices(pgPolygonObject *self, void *closure)
     return _pg_polygon_vertices_aslist(&self->polygon);
 }
 
+static PyObject *
+pg_polygon_get_center_x(pgPolygonObject *self, void *closure)
+{
+    return PyFloat_FromDouble(self->polygon.c_x);
+}
+
+static int
+pg_polygon_set_center_x(pgPolygonObject *self, PyObject *value, void *closure)
+{
+    double val;
+    DEL_ATTR_NOT_SUPPORTED_CHECK_NO_NAME(value);
+    if (!pg_DoubleFromObj(value, &val)) {
+        PyErr_SetString(PyExc_TypeError, "Expected a number");
+        return -1;
+    }
+    move_polygon(&(self->polygon), (val - self->polygon.c_x), 0.0);
+    return 0;
+}
+
+static PyObject *
+pg_polygon_get_center_y(pgPolygonObject *self, void *closure)
+{
+    return PyFloat_FromDouble(self->polygon.c_y);
+}
+
+static int
+pg_polygon_set_center_y(pgPolygonObject *self, PyObject *value, void *closure)
+{
+    double val;
+    DEL_ATTR_NOT_SUPPORTED_CHECK_NO_NAME(value);
+    if (!pg_DoubleFromObj(value, &val)) {
+        PyErr_SetString(PyExc_TypeError, "Expected a number");
+        return -1;
+    }
+    move_polygon(&(self->polygon), 0.0, (val - self->polygon.c_y));
+    return 0;
+}
+
+static PyObject *
+pg_polygon_get_center(pgPolygonObject *self, void *closure)
+{
+    return pg_tuple_from_values_double(self->polygon.c_x, self->polygon.c_y);
+}
+
+static int
+pg_polygon_set_center(pgPolygonObject *self, PyObject *value, void *closure)
+{
+    double new_c_x = 0, new_c_y = 0;
+    DEL_ATTR_NOT_SUPPORTED_CHECK_NO_NAME(value);
+    if (!pg_TwoDoublesFromObj(value, &new_c_x, &new_c_y)) {
+        PyErr_SetString(PyExc_TypeError, "Expected a sequence of 2 numbers");
+        return -1;
+    }
+    move_polygon(&(self->polygon), (new_c_x - self->polygon.c_x),
+                 (new_c_y - self->polygon.c_y));
+    return 0;
+}
+
 static PyGetSetDef pg_polygon_getsets[] = {
+    {"c_x", (getter)pg_polygon_get_center_x, (setter)pg_polygon_set_center_x,
+    NULL, NULL},
+    {"c_y", (getter)pg_polygon_get_center_y, (setter)pg_polygon_set_center_y,
+    NULL, NULL},
+    {"center", (getter)pg_polygon_get_center, (setter)pg_polygon_set_center,
+    NULL, NULL},
     {"verts_num", (getter)pg_polygon_get_verts_num, NULL,
      "Number of vertices of the polygon", NULL},
     {"vertices", (getter)pg_polygon_get_vertices, NULL,
--- test/test_polygon.py
+++ test/test_polygon.py
@@ -1,6 +1,7 @@
+from tkinter import CENTER
 import unittest
 
-from pygame import Vector2
+from pygame import Vector2, Vector3
 
 from geometry import Polygon
 
@@ -8,6 +9,7 @@ p1 = (12.0, 12.0)
 p2 = (32.0, 43.0)
 p3 = (22.0, 4.0)
 p4 = (332.0, 64.0)
+_some_vertices = [[10.0, 10.0], [20.0, 20.0], [30.0, 10.0]]
 
 
 class PolygonTypeTest(unittest.TestCase):
@@ -124,20 +126,205 @@ class PolygonTypeTest(unittest.TestCase):
                 po.copy(*value)
 
     def test_copy_return_type(self):
-        """Checks whether the copy method returns a polygon"""
+        """Checks whether the copy method returns a polygon."""
         po = Polygon([p1, p2, p3, p4])
 
         self.assertIsInstance(po.copy(), Polygon)
         self.assertEqual(type(po.copy()), Polygon)
 
     def test_copy(self):
-        """Checks whether the copy method works correctly"""
+        """Checks whether the copy method works correctly."""
         po = Polygon([p1, p2, p3, p4])
         po_2 = po.copy()
 
         self.assertEqual(po_2.vertices, [p1, p2, p3, p4])
         self.assertEqual(po_2.vertices, po.vertices)
 
+    def test_center_x(self):
+        """Makes sure changing center x component does change the positions of vertices properly."""
+        vertices = _some_vertices.copy()
+        poly = Polygon(vertices)
+        poly_c_x = poly.c_x
+        poly.c_x = 100.0
+        for vertex in vertices:
+            vertex[0] += 100.0 - poly_c_x
+
+        vertices = [tuple(vertex) for vertex in vertices]
+        self.assertEqual(poly.vertices, vertices)
+
+    def test_center_x__invalid_value(self):
+        """Ensures the function can handle the polygon center x component by invalid data types."""
+        poly = Polygon(_some_vertices.copy())
+        for value in (None, [], "1", (1,), [1, 2, 3]):
+            with self.assertRaises(TypeError):
+                poly.c_x = value
+    
+    def test_center_x__del(self):
+        """Ensures that x component cannot be deleted."""
+        poly = Polygon(_some_vertices.copy())
+        with self.assertRaises(AttributeError):
+            del poly.c_x
+
+    def test_center_y(self):
+        """Makes sure changing center y component does change the positions of vertices properly."""
+        vertices = _some_vertices.copy()
+        poly = Polygon(vertices)
+        poly_c_y = poly.c_y
+        poly.c_y = 100.0
+        for vertex in vertices:
+            vertex[1] += 100.0 - poly_c_y
+
+        vertices = [tuple(vertex) for vertex in vertices]
+        self.assertEqual(poly.vertices, vertices)
+
+    def test_center_y__invalid_value(self):
+        """Ensures the function can handle the polygon center y component by invalid data types."""
+        poly = Polygon(_some_vertices)
+        for value in (None, [], "1", (1,), [1, 2, 3]):
+            with self.assertRaises(TypeError):
+                poly.c_y = value
+
+    def test_center_y__del(self):
+        """Ensures that y component cannot be deleted."""
+        poly = Polygon(_some_vertices)
+        with self.assertRaises(AttributeError):
+            del poly.c_y
+
+    def test_center(self):
+        """Makes sure that setting new center moves the vertices properly."""
+        poly = Polygon(_some_vertices.copy())
+        poly_center = poly.center
+        poly.center = (200.0, 200.0)
+        vertices = _some_vertices.copy()
+        for vertex in vertices:
+            vertex[0] += 200.0 - poly_center[0]
+            vertex[1] += 200.0 - poly_center[1]
+
+        vertices = [tuple(vertex) for vertex in vertices]
+        self.assertEqual(poly.vertices, vertices)
+        self.assertEqual(poly.c_x, 200.0)
+        self.assertEqual(poly.c_y, 200.0)
+
+    def test_center__invalid_value(self):
+        """Ensures the function can handle the polygon center component by invalid data types."""
+        poly = Polygon(_some_vertices.copy())
+        for value in (None, [], "1", (1,), [1, 2, 3]):
+            with self.assertRaises(TypeError):
+                poly.center = value
+
+    def test_center__del(self):
+        """Ensures that center component cannot be deleted."""
+        poly = Polygon(_some_vertices.copy())
+        with self.assertRaises(AttributeError):
+            del poly.center
+
+    def test_move(self):
+        """Checks whether polygon moved correctly."""
+        poly = Polygon(_some_vertices.copy())
+        center_x = poly.c_x
+        center_y = poly.c_y
+
+        new_poly = poly.move(10.0, 10.0)
+        vertices = _some_vertices.copy()
+        
+        for vertex in vertices:
+            vertex[0] += 10.0
+            vertex[1] += 10.0
+
+        vertices = [tuple(vertex) for vertex in vertices]
+        self.assertEqual(vertices, new_poly.vertices)
+        self.assertEqual(new_poly.c_x, center_x + 10.0)
+        self.assertEqual(new_poly.c_y, center_y + 10.0)
+
+    def test_move_inplace(self):
+        """Checks whether polygon moved by (0, 0) and is the returned polygon identical."""
+        poly = Polygon(_some_vertices.copy())
+        poly_new = poly.move(0, 0)
+
+        ##self.assertAlmostEquals(poly_new, poly)
+        self.assertEqual(poly_new.c_x, poly.c_x)
+        self.assertEqual(poly_new.c_y, poly.c_y)
+        self.assertEqual(poly_new.vertices, poly.vertices)
+
+    def test_move_invalid_args(self):
+        """Tests whether function can handle invalid parameter types correctly."""
+        vertices = _some_vertices.copy()
+        invalid_types = (None, [], "1", (1,), Vector3(1, 1, 3), Polygon(vertices))
+
+        poly = Polygon(vertices)
+
+        for value in invalid_types:
+            with self.assertRaises(TypeError):
+                poly.move(value)
+
+    def test_move_argnum(self):
+        poly = Polygon(_some_vertices.copy())
+
+        invalid_args = [(1, 1, 1), (1, 1, 1, 1)]
+
+        for arg in invalid_args:
+            with self.assertRaises(TypeError):
+                poly.move(*arg)
+
+    def test_move_return_type(self):
+        poly = Polygon(_some_vertices.copy())
+
+        self.assertIsInstance(poly.move(1, 1), Polygon)
+
+    def test_move_ip(self):
+        """Ensures that the vertices are moved correctly"""
+        vertices = _some_vertices.copy()
+        poly = Polygon(vertices)
+        center_x = poly.c_x
+        center_y = poly.c_y
+
+        poly.move_ip(10.0, 10.0)
+        for vertex in vertices:
+            vertex[0] += 10.0
+            vertex[1] += 10.0
+
+        vertices = [tuple(vertex) for vertex in vertices]
+        self.assertEqual(poly.vertices, vertices)
+        self.assertEqual(poly.c_x, center_x + 10.0)
+        self.assertEqual(poly.c_y, center_y + 10.0)
+
+    def test_move_ip_inplace(self):
+        """Ensures that moving the polygon by (0, 0) will not change its position."""
+        poly = Polygon(_some_vertices.copy())
+        vertices = _some_vertices.copy()
+        center_x = poly.c_x
+        center_y = poly.c_y
+        
+        poly.move_ip(0, 0)
+
+        vertices = [tuple(vertex) for vertex in vertices]
+        self.assertEqual(poly.vertices, vertices)
+        self.assertEqual(poly.c_x, center_x)
+        self.assertEqual(poly.c_y, center_y)
+
+    def test_move_ip_return_type(self):
+        poly = Polygon(_some_vertices.copy())
+
+        self.assertEqual(type(poly.move_ip(0, 0)), type(None))
+
+    def test_move_ip_invalid_args(self):
+        """tests if the function correctly handles incorrect types as parameters"""
+        invalid_types = (None, [], "1", (1,), Vector3(1, 1, 3), Polygon(_some_vertices.copy()))
+
+        poly = Polygon(_some_vertices.copy())
+
+        for value in invalid_types:
+            with self.assertRaises(TypeError):
+                poly.move_ip(value)
+
+    def test_move_ip_argnum(self):
+        poly = Polygon(_some_vertices.copy())
+
+        invalid_args = [(1, 1, 1), (1, 1, 1, 1)]
+
+        for arg in invalid_args:
+            with self.assertRaises(TypeError):
+                poly.move_ip(*arg)
 
 if __name__ == "__main__":
     unittest.main()
