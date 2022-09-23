a58c3dea0549caf21a6246073cbc20abacf343b6
Emc2356 <63981925+Emc2356@users.noreply.github.com>
2022-09-24 02:51:24 +0300

    now it rebuilds

--- setup.py
+++ setup.py
@@ -48,12 +48,22 @@ def get_times_json_file() -> Path:
     return path
 
 
+def update_times_file() -> None:
+    files = list(Path("./src_c/").glob("**/*.[c|h]"))
+
+    data: Dict[str, float] = {str(file): os.path.getmtime(str(file)) for file in files}
+
+    with open(get_times_json_file(), "w") as f:
+        f.truncate(0)
+        f.write(json.dumps(data, indent=4))
+
+
 def needs_to_be_rebuild() -> bool:
     files = list(Path("./src_c/").glob("**/*.[c|h]"))
 
     with open(get_times_json_file(), "r+") as f:
         file_contents = f.read()
-        data: Dict[str, str]
+        data: Dict[str, float]
 
         if file_contents == "":
             data = {}
@@ -86,10 +96,17 @@ def build() -> None:
     if not needs_to_be_rebuild():
         return
 
+    with open("src_c/geometry.c", "rb+") as f:
+        f.write(b"")
+
     setup(
         name="geometry",
         ext_modules=extensions,
     )
+
+    # we are updating the times because we changed geometry.c
+    # to rebuild the project
+    update_times_file()
 
 
 def main() -> None:
