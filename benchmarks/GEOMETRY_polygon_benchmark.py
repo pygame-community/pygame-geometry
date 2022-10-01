cafc5a7e18f089e02b9b9924aef19454617a21bc
Alberto <103119829+itzpr3d4t0r@users.noreply.github.com>
2022-10-02 00:03:13 +0200

    New polygon creation method (#114)
    * added _pg_polygon_subtype_new2_transfer
    
    * added move, move_ip, setters and getters test

--- benchmarks/GEOMETRY_polygon_benchmark.py
+++ benchmarks/GEOMETRY_polygon_benchmark.py
@@ -1,6 +1,6 @@
 from benchmark_utils import TestSuite
 
-from geometry import Polygon
+from geometry import Polygon, regular_polygon
 
 # === Tests ===
 # Each test consists of a tuple of: (name, call)
@@ -15,6 +15,7 @@ GLOB = {
     "Polygon": Polygon,
     "po3": Polygon([(50, 50), (50, 100), (70, 55)]),
     "po4": Polygon([(50, 50), (50, 100), (70, 55), (100, 23)]),
+    "po100": regular_polygon(100, (50, 50), 50),
     "p1_i": (50, 50),
     "p2_i": (50, 100),
     "p3_i": (70, 55),
@@ -47,11 +48,23 @@ getters_tests = [
     ("verts_num 3", "po3.verts_num"),
     ("vertices 4", "po4.vertices"),
     ("verts_num 4", "po4.verts_num"),
+    ("c_x 3", "po3.c_x"),
+    ("c_y 3", "po3.c_y"),
+    ("c_x 4", "po4.c_x"),
+    ("c_y 4", "po4.c_y"),
+    ("center 3", "po3.center"),
+    ("center 4", "po4.center"),
 ]
 
 setters_tests = [
-    ("vertices int", "po4.vertices = [p1_i, p2_i, p3_i, p4_i]"),
-    ("vertices float", "po4.vertices = [p1_f, p2_f, p3_f, p4_f]"),
+    # ("vertices int", "po4.vertices = [p1_i, p2_i, p3_i, p4_i]"),
+    # ("vertices float", "po4.vertices = [p1_f, p2_f, p3_f, p4_f]"),
+    ("center int", "po4.center = p1_i"),
+    ("center float", "po4.center = p1_f"),
+    ("c_x int", "po4.c_x = 50"),
+    ("c_y int", "po4.c_y = 50"),
+    ("c_x float", "po4.c_x = 50.0"),
+    ("c_y float", "po4.c_y = 50.0"),
 ]
 
 copy_tests = [
@@ -59,10 +72,31 @@ copy_tests = [
     ("copy 4", "po4.copy()"),
 ]
 
+move_tests = [
+    ("move 100 tuple int", "po100.move((10, 10))"),
+    ("move 100 tuple float", "po100.move((10.0, 10.0))"),
+    ("move 100 2 int", "po100.move(10, 10)"),
+    ("move 100 2 float", "po100.move(10.0, 10.0)"),
+    ("move 100 2 int", "po100.move(10, 10)"),
+    ("move 100 2 float", "po100.move(10.0, 10.0)"),
+]
+
+move_ip_tests = [
+    ("move_ip 100 tuple int", "po100.move_ip((10, 10))"),
+    ("move_ip 100 tuple float", "po100.move_ip((10.0, 10.0))"),
+    ("move_ip 100 2 int", "po100.move_ip(10, 10)"),
+    ("move_ip 100 2 float", "po100.move_ip(10.0, 10.0)"),
+    ("move_ip 100 2 int", "po100.move_ip(10, 10)"),
+    ("move_ip 100 2 float", "po100.move_ip(10.0, 10.0)"),
+]
+
 GROUPS = [
     ("Creation", creation_tests),
     ("Attribute Getters", getters_tests),
+    ("Attribute Setters", setters_tests),
     ("Copy", copy_tests),
+    ("Move", move_tests),
+    ("Move_ip", move_ip_tests),
 ]
 
 if __name__ == "__main__":
